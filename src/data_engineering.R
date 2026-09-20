13f36702051141eeac66143aa5901c2d6b375dc8
Maja-tech04 <m.grzesik@tilburguniversity.edu>
2026-09-20 12:45:23 +0200

    Add timestamp conversion and lagged daily metrics

--- src/data_engineering.R
+++ src/data_engineering.R
@@ -102,5 +102,31 @@ creator_event_summary <- watch_log %>%
 
 # Exercise 5
 
+watch_time_preview <- watch_log %>%
+  mutate(
+    shown_ts = as.POSIXct(shown_at,
+                          format = "%Y-%m-%dT%H:%M:%SZ",
+                          tz = "UTC"),
+    shown_day = as.Date(shown_ts)
+  ) %>%
+  select(impression_id, creator_id, shown_at, shown_ts, shown_day) %>%
+  head(8)
+
+creator_daily <- watch_log %>%
+  mutate(
+    shown_ts = as.POSIXct(shown_at, format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC"),
+    shown_day = as.Date(shown_ts)
+  ) %>%
+  count(creator_id, shown_day, name = "impressions_n")
+
+creator_daily <- creator_daily %>%
+  group_by(creator_id) %>%
+  arrange(shown_day) %>%
+  mutate(
+    impressions_lag1 = lag(impressions_n),
+    impressions_change = impressions_n - impressions_lag1
+  )
+
+write_csv(creator_daily, "temp/creator_daily_week4.csv")
 
 
