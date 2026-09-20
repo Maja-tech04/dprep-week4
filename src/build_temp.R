3b0d8492707502b89641e63293da0da3acb8f9c7
Maja-tech04 <m.grzesik@tilburguniversity.edu>
2026-09-20 13:31:08 +0200

    Add build scripts for temp and output tables

--- src/build_temp.R
+++ src/build_temp.R
@@ -3,3 +3,15 @@
 library(tidyverse)
 
 dir.create("temp", showWarnings = FALSE)
+
+video_view <- read_csv("video_view.csv")
+creators <- read_csv("creators.csv")
+
+creator_week4 <- video_view %>%
+  group_by(creator_id) %>%
+  summarise(
+    videos_n = n(),
+    impressions_total = sum(impressions_n, na.rm = TRUE),
+    watched_total = sum(watched_n, na.rm = TRUE),
+    avg_watch_rate = mean(watch_rate, na.rm = TRUE) 
+  )
